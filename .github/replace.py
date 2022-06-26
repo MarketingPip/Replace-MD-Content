b3909cd0d8333389802fbcf5665921efcf2ffe5c
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-26 18:40:30 -0400

    Update replace.py

--- .github/replace.py
+++ .github/replace.py
@@ -1,9 +1,16 @@
+Template = """1. ❗️ Opened issue [#11](https://github.com/) in [Meow](https://github.com)
+2. ❗️ Closed issue [#9](https://github.com/) in [Meow](https://github.com)
+3. 🎉 Merged PR [#10](https://github.com/) in [Meow](https://github.com)
+4. 💪 Opened PR [#10](https://github.com/) in [Meow](https://github.com)
+5. ❗️ Opened issue [#9](https://github.com/) in [Meow](https://github.com)"""
+
+
 with open('README.md', 'r') as f:
     contents = f.read()
     starting_text = '<---START OF CONTENT --->'
     ending_text = '<---END OF CONTENT --->'
     to_replace = contents[contents.find(starting_text)+len(starting_text):contents.rfind(ending_text)]
-    contents = contents.replace(to_replace, 'whatever you want to replace it with')
+    contents = contents.replace(to_replace, TEMPLATE)
     
 with open('README.md', 'w') as f:
     f.write(contents)   

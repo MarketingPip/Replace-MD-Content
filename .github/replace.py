4d7af37d9230bfc543a8932b7ab61a956018e302
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-26 20:03:10 -0400

    Update replace.py

--- .github/replace.py
+++ .github/replace.py
@@ -22,6 +22,7 @@ with open(FileName, 'r') as f:
     # Define the second line where your content will be replaced / added 
     ending_text = '<!---END OF CONTENT --->'
     to_replace = contents[contents.find(starting_text)+len(starting_text):contents.rfind(ending_text)]
+    print(to_replace)
     contents = contents.replace(to_replace, Template)
     
 with open(FileName, 'w') as f:

2fc7f195e39a211eeb54e17d1205d27359bf5fe1
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-12 03:49:27 -0400

    Update replace.py

--- .github/replace.py
+++ .github/replace.py
@@ -5,24 +5,28 @@ import re as regex
 ## The content you want to add to your README / Replace in between two sections (JSON values etc)
 # Note: Do not forget to add a line break (empty line) at the top and bottom
 Template = """
+<!---START OF CONTENT --->
 1. ❗️ Opened issue [#11](https://github.com/) in [Meow](https://github.com)
 2. ❗️ Closed issue [#9](https://github.com/) in [Meow](https://github.com)
 3. 🎉 Merged PR [#10](https://github.com/) in [Meow](https://github.com)
 4. 💪 Opened PR [#10](https://github.com/) in [Meow](https://github.com)
 5. ❗️ Opened issue [#9](https://github.com/) in [Meow](https://github.com)
-
+<!---END OF CONTENT --->
 """
 # Define the filename here you want to replace content in
 FileName = "README.md"
 
 with open(FileName, 'r') as f:
     contents = f.read()
+    for line in contents:
+        contents = re.sub("<!---START OF CONTENT ---> \S+ <!---END OF CONTENT --->", Template, line)
     # Define the first line where your content will be replaced / added 
-    starting_text = '<!---START OF CONTENT --->'
+ #   starting_text = '<!---START OF CONTENT --->'
     # Define the second line where your content will be replaced / added 
-    ending_text = '<!---END OF CONTENT --->'
-    to_replace = contents[contents.find(starting_text)+len(starting_text):contents.find(ending_text)]
-    contents = contents.replace(to_replace, Template)
+  #  ending_text = ''
+    
+   # to_replace = contents[contents.find(starting_text)+len(starting_text):contents.find(ending_text)]
+    #contents = contents.replace(to_replace, Template)
     
 with open(FileName, 'w') as f:
     f.write(contents)   
